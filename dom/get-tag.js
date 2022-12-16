1b63409eb83c9781e0ab69ae71c176013e58a857
salih <salihkilinc80@gmail.com>
2022-12-16 18:58:28 +0100

    v7

---
+++ dom/get-tag.js
@@ -0,0 +1,14 @@
+// let intro1 = document.getElementById("intro1")
+// let message = document.getElementById("message")
+// message.innerHTML = intro1.innerHTML;
+
+// let allList = document.getElementsByTagName("ul");
+// let citys = allList[0];
+
+// allEle = citys.getElementsByTagName("li");
+
+// for (i = 0; i < allEle.length; i++) {
+//     alert(allEle[i].innerHTML)
+// }
+let queryEl = document.querySelectorAll("p.intro1");
+alert(queryEl.length);
