ac4a36c701140adb998bf9be6f7161d0bc195220
salih <salihkilinc80@gmail.com>
2022-12-17 17:22:29 +0100

    v8

--- dom/get-tag.js
+++ dom/get-tag.js
@@ -10,5 +10,17 @@
 // for (i = 0; i < allEle.length; i++) {
 //     alert(allEle[i].innerHTML)
 // }
-let queryEl = document.querySelectorAll("p.intro1");
-alert(queryEl.length);
+// let queryEl = document.querySelectorAll("p.intro1");
+// alert(queryEl.length);
+
+// let users = document.getElementsByName("client-Name");
+// alert(users[0].value);
+
+let Mehmet = document.getElementById("Mehmet").addEventListener("click", rengiDegistir); // ismin uzerine geldigi zaman kilinc olan event fonction
+
+function rengiDegistir() {
+    document.getElementById("div1").style.color = "blue";
+    let users = document.getElementsByName("client-Name");
+    users[0].value = "KILINC";
+
+}
