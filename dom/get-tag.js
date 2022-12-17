e2f0e370eccaa914b8c5f7cd13cd676fcf288b4d
salih <salihkilinc80@gmail.com>
2022-12-17 18:08:11 +0100

    v9

--- dom/get-tag.js
+++ dom/get-tag.js
@@ -16,11 +16,27 @@
 // let users = document.getElementsByName("client-Name");
 // alert(users[0].value);
 
-let Mehmet = document.getElementById("Mehmet").addEventListener("click", rengiDegistir); // ismin uzerine geldigi zaman kilinc olan event fonction
+// let Mehmet = document.getElementById("Mehmet").addEventListener("click", rengiDegistir); // ismin uzerine geldigi zaman kilinc olan event fonction
 
-function rengiDegistir() {
-    document.getElementById("div1").style.color = "blue";
-    let users = document.getElementsByName("client-Name");
-    users[0].value = "KILINC";
+// function rengiDegistir() {
+//     document.getElementById("div1").style.color = "blue";
+//     let users = document.getElementsByName("client-Name");
+//     users[0].value = "KILINC";
 
-}
+// }
+
+
+// let node = document.getElementById("tree");
+// alert(node.childNodes[0].nodeValue)
+
+let heading = document.createElement("h2");
+let node = document.createTextNode("Bonjour Javascript")
+heading.appendChild(node)
+
+let div1 = document.getElementById("div1")
+let p2 = document.getElementById("p2")
+
+div1.insertBefore(heading,p2)
+
+alert("p2 siliniyor dikkat edin")   ///p2 tagini sildik
+div1.removeChild(p2)
