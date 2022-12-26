753c2aeaf7785950f6f008fdc94058567f272cfa
salih <salihkilinc80@gmail.com>
2022-12-26 21:30:40 +0100

    v6

---
+++ you-pagege/assets/data/students.js
@@ -0,0 +1,6 @@
+export const students = [
+    {id: 1, name :"Mehmet" , point :96},
+    {id: 2, name :"Selma" , point :97},
+    {id: 3, name :"Semra" , point :95},
+    {id: 4, name :"Kenan" , point :80},
+]
---
+++ you-pagege/assets/js/tablo.js
@@ -0,0 +1,66 @@
+import { students } from "../data/students.js";
+
+const letterInput = document.getElementById("textInput");
+const btnAdd = document.getElementById("btnAdd");
+const point = document.getElementById("btnPoint");
+const tblStudentsTbody = document.querySelector("#tblStudents tbody");
+
+const loadData = () => {
+  let strHtml = "";
+  students.forEach((student, index) => {
+    strHtml += `<tr>
+        <th scope="row">${index + 1}</th>
+        <td>${student.name}</td>
+        <td class="score">${student.point}</td>
+        <td><button class="btn-degistir btn btn-warning"><i class="fa-solid fa-pen"></i></button>
+        <button class="btn-add btn btn-warning d-none"><i class="fa-solid fa-check"></i></button>
+        <button class="btn-delete btn btn-danger">🗑️</button>
+        </button>
+        <button class="btn-carpım btn btn-danger d-none"><i class="fa-solid fa-xmark"></i></button>
+        </td>
+      </tr>`;
+  });
+  tblStudentsTbody.innerHTML = strHtml;
+};
+loadData();
+/* EVENTS */
+document.getElementById("btnShowLowScores").addEventListener("click", () => {
+  const lastTDs = tblStudentsTbody.querySelectorAll("tr td.score");
+  console.log(lastTDs);
+  lastTDs.forEach((td, index) => {
+    if (td.innerText < 50) {
+      //td.style.backgroundColor = "red";
+      tblStudentsTbody.querySelector(
+        `tr:nth-child(${index + 1})`
+      ).style.backgroundColor = "red";
+    }
+  });
+});
+document.querySelectorAll(".btn-delete").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.stopPropagation(); // olayın parent lara aktarılmasını engeller.
+    const trEl = e.target.closest("tr");
+    const name = trEl.querySelector("td").innerText;
+
+    const result = confirm(`Are you sure to delete ${name}?`);
+
+    if (result) {
+      setTimeout(() => {
+        trEl.remove();
+      }, 1000);
+    }
+  });
+});
+tblStudentsTbody.querySelectorAll("tr").forEach((tr) => {
+  tr.addEventListener("click", (e) => {
+    e.target.closest("tr").classList.toggle("table-info");
+  });
+});
+
+document.getElementById("btnAdd").addEventListener("click", () => {
+  let newTd = document.createElement("td");
+  newTd.innerHTML = letterInput.value;
+  tblStudentsTbody.appendChild(newTd);
+});
+
+/* EVENTS */
